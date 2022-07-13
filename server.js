3de2150a85b690df6109123a1e9cb852910ddf8f
ABBGamer <101691034+ABBGamer@users.noreply.github.com>
2022-07-13 10:35:04 +0700

    Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 const path = require('path');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 var app_path = './dist/test_task_ccrs';
 app.use('/', express.static(path.join(__dirname,app_path)))
   .get('*',(req,res)=>res.sendFile(path.join(__dirname,app_path +'/index.html')))
